d1aed33ae29120c72269dafc9b4234572bea6ec3
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-19 09:50:19 -0500

    edited knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,41 +4,15 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3',
-    },
-    useNullAsDefault: true
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './data/car-dealer.db3' // path for db
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: './data/migrations' // path for migrations folder
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: './data/seeds' // path for seeds folder
     }
   }
 
